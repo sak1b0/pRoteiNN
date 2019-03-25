e4c9f8e59ee62e6aa4296bf2968aa17773078b3f
Sakib <sakibultimate@gmail.com>
2019-03-25 23:40:21 +0600

    datasets are good to go

--- converter.py
+++ converter.py
@@ -3,17 +3,21 @@ Created on Mon Mar 25 21:42:17 2019
 
 @author: Sakib
 """
-f = open("train.txt", "r")
-o = open("train_formatted.csv", "a")
+f = open("test.txt", "r")
+o = open("test_formatted.csv", "a")
 
 
-for item in range (2):
+while True:
     first=f.readline()
+    if first == '':
+        break
     second=f.readline()
     third=f.readline()
     out=second.rstrip()+","+third
-    print(out)
+    #print(out)
     o.write(out)
+   
 
 f.close()
 o.close()
+print("Done!")
